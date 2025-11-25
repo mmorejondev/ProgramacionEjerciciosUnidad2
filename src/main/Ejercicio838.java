811eae121d0f3308c984a1af03dd4d265a28ddea
mmorejondev <mmorejondev@gmail.com>
2025-11-25 21:34:12 +0100

    Añadido el ejercicio Ejercicio838 usando obligatoriamente el método buscarElemento(int [] m,int a) del ejercicio Ejercicio837

--- src/main/Ejercicio838.java
+++ src/main/Ejercicio838.java
@@ -17,39 +17,37 @@ public class Ejercicio838 {
 		
 		int [] unicos = new int[duplicados.length];
 	
-		boolean encontrado=false;
+		int j=0; //Con este índice recorreremos el array de únicos
+		boolean ceroInsertado=false;
 		
+		/*Recorremos el array de duplicados*/
 		for (int i=0;i<duplicados.length;i++) {
-			encontrado=buscarElemento(unicos,duplicados[i]);
-			if (encontrado==false) {
-				   unicos[i]=duplicados[i];
-			   }else {
-				   unicos[i]=Integer.MAX_VALUE;
-			   }
-		}
-		int contadorUnicos=0;
-		for (int i=0;i<unicos.length;i++) {
-			if (unicos[i]!=Integer.MAX_VALUE) {
-				contadorUnicos++;
+			
+			/*Tenemos en cuenta el caso especial del cero, ya que aunque puede formar parte de los números que nos interesan, también está repetido en el array de unicos*/
+			/*Si en el array de duplicados nos encontramos con un cero y aún no hemos insertado ninguno en únicos, cambiamos el valor del booleano y añadimos el 0 al array de unicos*/
+			if (duplicados[i]==0 && !ceroInsertado) { //Aquí sólo va a entrar en la primera aparición de un 0 en duplicados
+				unicos[j]=0; //Añadimos el cero al array de únicos. Esto sólo ocurrirá una vez
+				j++; //Incrementamos el número de elementos insertados
+				ceroInsertado=true; //Cambiamos el valor de la bandera para que no vuelva a entrar en este condicional para el resto de ceros
+				
+							
+			}else { // El elemento de duplicados no es cero
+				if (!buscarElemento(unicos,duplicados[i])) {  /*Buscamos el elemento en el array de unicos, usando el método del ejercicio anterior*/
+						unicos[j]=duplicados[i]; //añadimos al array de unicos
+						j++; //incrementamos la cantidad de elementos unicos
+					}
+				}	
+			
 			}
-		}
+			
+			
+		int [] unicosRecortado = new int [j];
+		for (int i=0;i<unicosRecortado.length;i++) {
+			unicosRecortado[i]=unicos[i];
+		}		
 		
-		int [] unicos2 = new int [contadorUnicos];
-		int j=0;
-		for (int i=0;i<unicos.length;i++) {
-			if (unicos[i]!=Integer.MAX_VALUE) {
-				unicos2[j]=unicos[i];
-				j++;
-			}
-		}
-		
-		
-		return unicos2;
+		return unicosRecortado;
 	}
-	
-
-
-	
 	
 	public static void main(String[] args) {
 		/*Ejercicio propuesto 8.3.8: Eliminar duplicados en un array. 
@@ -62,7 +60,7 @@ public class Ejercicio838 {
 		 * Usar un método similar al creado en el ejercicio anterior 
 		 * para determinar si un elemento ya está en el array.
 		 */
-		int [] conDuplicados = {7,3,6,5,7,1,3,3};
+		int [] conDuplicados = {0,0,0,0,0,12,7,3,6,0,5,7,6,6,6,2,2,1,3,3,0,0};
 		for (int i=0;i<conDuplicados.length;i++) {
 			System.out.print("["+ conDuplicados[i] +"]");
 		}
